9c3b8ac4159695fe1e78329df28d40fcd7e98763
Arthur <124591050+arthurhhenz@users.noreply.github.com>
2023-02-15 11:39:07 -0300

    Create hideKeyboard.kt

---
+++ hideKeyboard.kt
@@ -0,0 +1,14 @@
+    fun Fragment.hideKeyboard() {
+        view?.let { activity?.hideKeyboard(it) }
+    }
+
+    fun Activity.hideKeyboard() {
+        hideKeyboard(currentFocus ?: View(this))
+    }
+
+    @SuppressLint("ServiceCast")
+    fun Context.hideKeyboard(view: View) {
+        val inputMethodManager =
+            getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
